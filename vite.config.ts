5af5b70fbb551d31dd63bc2848f8fe5114a544c2
Kasia Szlejter <katarzyna.szlejter@bouvet.no>
2026-09-11 10:05:35 +0200

    Dockerize app

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host:true
+  },
+  preview: {
+    port: 5173,
+    host:true
+  }
 })
